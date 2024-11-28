0a076455300096f4cd90e0b6c0722d7d7a7cd225
JosephNderitu <123462797+JosephNderitu@users.noreply.github.com>
2024-11-28 13:37:57 +0300

    Update Main.java

--- BankingSystem AP assignment1/src/Main.java
+++ BankingSystem AP assignment1/src/Main.java
@@ -1,20 +1,30 @@
 public class Main {
-    public static void main(String[] args) throws InsufficientFundsException {  // Declare exception
+    public static void main(String[] args) throws InsufficientFundsException {
+        // Create a bank account and register it
         BankAccount account = new BankAccount(1000);
+        BankAccountManager.setAccountInstance(account);
 
         // Test deposit
         TransactionInterface deposit = new DepositTransaction(500);
         deposit.apply(account);
         deposit.printTransactionDetails();
+        System.out.println("Balance after deposit: " + account.getBalance());
 
         // Test withdrawal
         WithdrawalTransaction withdrawal = new WithdrawalTransaction(300);
-        withdrawal.apply(account);  // No try-catch needed because exception is declared in main
+        withdrawal.apply(account);  // Withdraw 300
         withdrawal.printTransactionDetails();
+        System.out.println("Balance after withdrawal: " + account.getBalance());
 
         // Test partial withdrawal
         WithdrawalTransaction largeWithdrawal = new WithdrawalTransaction(2000);
         largeWithdrawal.apply(account, true);
+        System.out.println("Balance after partial withdrawal: " + account.getBalance());
+
+        // Test reversal
+        boolean reversed = withdrawal.reverse(); // Reverse the withdrawal
+        System.out.println("Reversal successful: " + reversed);
+        System.out.println("Balance after reversal: " + account.getBalance());
 
         // Test type casting
         BaseTransaction baseTransaction = (BaseTransaction) withdrawal;
