1d6a01c621972c8b40395a0fdc46d6d53a4ad1aa
JosephNderitu <123462797+JosephNderitu@users.noreply.github.com>
2025-01-27 13:52:10 +0300

    Update TransactionInterface.java
    Added specifications

--- BankingSystem AP assignment1/src/TransactionInterface.java
+++ BankingSystem AP assignment1/src/TransactionInterface.java
@@ -1,9 +1,36 @@
 import java.util.Calendar;
 
+/**
+ * Interface for defining a transaction.
+ */
 public interface TransactionInterface {
+    /**
+     * Get the amount involved in the transaction.
+     * @return the amount as a double.
+     */
     double getAmount();
+
+    /**
+     * Get the date of the transaction.
+     * @return the date as a Calendar object.
+     */
     Calendar getDate();
+
+    /**
+     * Get the unique transaction ID.
+     * @return the transaction ID as a String.
+     */
     String getTransactionID();
+
+    /**
+     * Print the details of the transaction.
+     */
     void printTransactionDetails();
+
+    /**
+     * Apply the transaction to the given bank account.
+     * @param ba the bank account to apply the transaction to.
+     * @throws InsufficientFundsException if there are insufficient funds for the transaction.
+     */
     void apply(BankAccount ba) throws InsufficientFundsException;
 }
