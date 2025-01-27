ed9936b10f6d20c7f61aaa18dcf5082360914c34
JosephNderitu <123462797+JosephNderitu@users.noreply.github.com>
2025-01-27 14:07:55 +0300

    Update DepositTransaction.java
    added specifications

--- BankingSystem AP assignment1/src/DepositTransaction.java
+++ BankingSystem AP assignment1/src/DepositTransaction.java
@@ -1,8 +1,28 @@
+/**
+ * Represents a deposit transaction.
+ * 
+ * This class extends the `BaseTransaction` and implements the functionality
+ * to deposit a specified amount into a bank account.
+ * 
+ * @since 1.0.0
+ */
 public class DepositTransaction extends BaseTransaction {
+    /**
+     * Constructs a new `DepositTransaction` with the specified amount.
+     * 
+     * @param amount the amount to be deposited. Must be a positive value.
+     */
     public DepositTransaction(double amount) {
         super(amount);
     }
 
+    /**
+     * Applies the deposit transaction to the specified bank account.
+     * 
+     * This method adds the transaction amount to the bank account's balance.
+     * 
+     * @param ba the `BankAccount` instance to which the deposit will be applied.
+     */
     @Override
     public void apply(BankAccount ba) {
         ba.setBalance(ba.getBalance() + amount);
