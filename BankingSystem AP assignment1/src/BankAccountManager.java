c4a8cd750d1e34d6b0d92f1e9d20d7232041dbc1
JosephNderitu <123462797+JosephNderitu@users.noreply.github.com>
2025-01-27 14:05:01 +0300

    Update BankAccountManager.java
    added specifications

--- BankingSystem AP assignment1/src/BankAccountManager.java
+++ BankingSystem AP assignment1/src/BankAccountManager.java
@@ -1,10 +1,30 @@
+/**
+ * Manages a single instance of a bank account for global access.
+ * 
+ * This class provides methods to set and retrieve a static `BankAccount` instance,
+ * ensuring controlled access to the account.
+ * 
+ * @since 1.0.0
+ */
 public class BankAccountManager {
-    private static BankAccount accountInstance;
+    private static BankAccount accountInstance; // The static instance of the bank account being managed
 
+    /**
+     * Sets the static `BankAccount` instance to the specified account.
+     * 
+     * @param account the `BankAccount` instance to be managed.
+     *                Must not be null.
+     */
     public static void setAccountInstance(BankAccount account) {
         accountInstance = account;
     }
 
+    /**
+     * Retrieves the static `BankAccount` instance.
+     * 
+     * @return the `BankAccount` instance currently managed.
+     * @throws IllegalStateException if no account instance has been set prior to the call.
+     */
     public static BankAccount getAccountInstance() {
         if (accountInstance == null) {
             throw new IllegalStateException("No account instance set!");
